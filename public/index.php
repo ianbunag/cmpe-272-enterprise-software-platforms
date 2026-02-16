9b6f52428eaee14a13892f9a8082da2f2a8590a5
Ian Buñag <jianbunag@gmail.com>
2026-02-15 17:16:35 -0800

    Add favicon and robots.txt

--- public/index.php
+++ public/index.php
@@ -20,7 +20,9 @@ try {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="robots" content="noindex, nofollow, noarchive">
     <title>John Ian Buñag | CMPE-272</title>
+    <link rel="icon" type="image/x-icon" href="/static/favicon.ico">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.min.css">
 </head>
 <body>
